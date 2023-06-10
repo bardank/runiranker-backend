a1bba1ac822c0039810b05159c1167af8189107a
bardan <bardankhadka@gmail.com>
2023-06-10 19:22:50 +0530

    merged

--- src/services/institution/institution.ts
+++ src/services/institution/institution.ts
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-import { Institution } from "../../types/institutionType";
-=======
 import  xlsx from 'xlsx';
 
 const excelToJson = async(file: Buffer) => {
@@ -12,4 +9,3 @@ const  institutionService ={
 }
 
 export default  institutionService;
->>>>>>> 91c34dceecbe4c9f28c418d2585bc1e37760f168
